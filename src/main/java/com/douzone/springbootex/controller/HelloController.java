adc3b4ac20a70a8d6282a20a652a7fa524743e62
haeyonghahn <youg1322@naver.com>
2020-03-26 12:15:50 +0900

    springbootex init commit

---
+++ src/main/java/com/douzone/springbootex/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.douzone.springbootex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "hello world";
+	}
+	
+	@RequestMapping("/hello2")
+	public String hello2() {
+		return "/WEB-INF/views/hello.jsp";
+	}
+}
